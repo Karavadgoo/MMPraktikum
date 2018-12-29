f18bb68a9fc98787fde70cec8af9589debb4c59f
Karavadgoo <karavadgoo@gmail.com>
2018-12-29 18:06:03 +0300

    Hope for the last change in solution of tasks of lesson 2

--- Lesson_02/Tasks/task_3.py
+++ Lesson_02/Tasks/task_3.py
@@ -1,19 +1,17 @@
 import numpy as np
 
 
+def f(Z):
+    isnan_column = np.isnan(Z)
+    if np.all(isnan_column):
+        Z = 0
+    else:
+        Z[isnan_column] = np.nanmean(Z)
+    return Z
+
+
 def replace_nan_to_means(X):
     Y = X.copy()
-
-    def f(Z):
-        if np.all(np.isnan(Z)):
-            Z[np.isnan(Z)] = 0
-        else:
-            Z[np.isnan(Z)] = np.nanmean(Z)
-        # try:
-            # Z[np.isnan(Z)] = np.nanmean(Z)
-        # except Exception:
-            # Z[np.isnan(Z)] = 0
-        return Z
     return np.apply_along_axis(f, 0, Y)
 
 
